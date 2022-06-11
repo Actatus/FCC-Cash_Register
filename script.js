0a65183fe7b7175e3e802b91dd07a8c6449dbca9
Actatus <itsactatus@yahoo.com>
2022-06-11 15:38:54 -0700

    Project creation commit.

---
+++ script.js
@@ -0,0 +1,7 @@
+//Design a register drawer function that takes a purchase price arg, cash given arg, and a Cash In Drawer argument.
+//Return an object with either 'Status: Open', Closed, or Insufficient_Funds
+
+function checkCashRegister(price, cash, cid) {
+  let change;
+  return change;
+}
