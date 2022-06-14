334e5cc8522a15a557f40aa896608eced41b6408
Actatus <itsactatus@yahoo.com>
2022-06-14 02:33:56 -0700

    Added additional unit tests for 20.50 back.

--- script.test.js
+++ script.test.js
@@ -2,7 +2,7 @@ const checkCashRegister = require('./script');
 
 const emptyRegister = [["PENNY", 0], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]];
 
-const variedRegister = [["PENNY", 1.01], ["NICKEL", 1.05], ["DIME", .7], ["QUARTER", 3.75], ["ONE", 15], ["FIVE", 20], ["TEN", 20], ["TWENTY", 49], ["ONE HUNDRED", 200]];
+const variedRegister = [["PENNY", 1.01], ["NICKEL", 1.05], ["DIME", .7], ["QUARTER", 3.75], ["ONE", 15], ["FIVE", 20], ["TEN", 20], ["TWENTY", 40], ["ONE HUNDRED", 200]];
 
 
 
@@ -14,6 +14,16 @@ test("Exact Change empty register", () => {
   expect(checkCashRegister(20, 20, emptyRegister)).toMatchObject({change: emptyRegister, status: "Closed"});
 });
 
-test("50c back", () => {
-  expect(checkCashRegister(20.5, 21, variedRegister)).toMatchObject({change: [["Quarter", .5]], status: "Open"});
+test("Exact change, varied register", () =>{
+  expect(checkCashRegister(20, 20, variedRegister)).toMatchObject({change: variedRegister, status: "Closed"});
 });
+
+test("50c back", () => {
+  expect(checkCashRegister(20.5, 21, variedRegister)).toMatchObject({change: [["QUARTER", .5]], status: "Open"});
+});
+
+test("$20.50 back", () =>{
+  expect(checkCashRegister(10, 35.50, variedRegister)).toMatchObject({change: [["TWENTY", 20], ["QUARTER", .5]]});
+});
+
+
