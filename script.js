6c8c7e696d8ac15360cfb6e57b3b5f37154535a3
Actatus <itsactatus@yahoo.com>
2022-06-14 01:30:13 -0700

    Added emptyDrawer default item, passes 2/3 tests.

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 //Design a register drawer function that takes a purchase price arg, cash given arg, and a Cash In Drawer argument.
 //Return an object with either 'Status: Open', Closed, or Insufficient_Funds
 
+const emptyDrawer = [
+  ["PENNY", 0],
+  ["NICKEL", 0],
+  ["DIME", 0],
+  ["QUARTER", 0],
+  ["ONE", 0],
+  ["FIVE", 0],
+  ["TEN", 0],
+  ["TWENTY", 0],
+  ["ONE HUNDRED", 0]
+];
 
 
-function checkCashRegister(price, cash, cid) {
+function checkCashRegister(price, cash, cid = emptyDrawer) {
   let registerStatus = {
     'status': "Closed",
     'change': []
@@ -13,6 +24,7 @@ function checkCashRegister(price, cash, cid) {
     let balance = price-cash;
     if (balance == 0){
       registerStatus.status = "Closed";
+      registerStatus.change = cid;
     } else if (balance < 0){
       registerStatus.status = "INSUFFICIENT_FUNDS";
     } else {
