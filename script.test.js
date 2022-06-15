d974910cea79e23cb823ffc50d1195f406678ae3
Actatus <itsactatus@yahoo.com>
2022-06-15 02:48:40 -0700

    Added 2 failed FCC unit tests.

--- script.test.js
+++ script.test.js
@@ -31,3 +31,25 @@ test("Open, change: 50c back", () =>{
     .toMatchObject({change: [["QUARTER", 0.5]], status: "OPEN"});
 });
 
+//checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], 
+//["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "INSUFFICIENT_FUNDS", 
+//change: []}
+
+test("INSUFFICIENT_FUNDS, not enough cash", () =>{
+  expect(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1],
+    ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])).toMatchObject({change: [], status:
+    "INSUFFICIENT_FUNDS"});
+});
+
+//checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], 
+//["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) 
+//should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], 
+//["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}
+
+test("Large cashback", () =>{
+  expect(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25],
+    ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])).toMatchObject({
+      change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1],["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]],
+      status: "OPEN"
+    });
+});
